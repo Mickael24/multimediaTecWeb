fa85d2431a6a0c5f51d594b2168401ff8316d77a
Mickael <dinomickael@gmail.com>
2017-03-15 19:43:15 +0000

    Fix router

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const server = http.createServer((req, res) => {
   res.end('Hello World\n');
 });
 */
-let router = require('./router');
+//let router = require('./router');
 var app = express();
 app.use(router.initialize());
 
